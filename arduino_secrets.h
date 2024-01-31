de5ba2c64f1a197ca94506357e99f8d6bc0bd750
LuisGuev <113613938+LuisGuev@users.noreply.github.com>
2024-01-31 13:36:37 -0600

    Add files via upload

---
+++ arduino_secrets.h
@@ -0,0 +1,15 @@
+// GSM settings
+#define SECRET_PINNUMBER     ""
+#define SECRET_GPRS_APN      "soracom.io" // replace your GPRS APN
+#define SECRET_GPRS_LOGIN    "sora"    // replace with your GPRS login
+#define SECRET_GPRS_PASSWORD "sora" // replace with your GPRS password
+
+// Fill in the hostname of your AWS IoT broker
+#define SECRET_BROKER "harvest.soracom.io"
+
+// Fill in the boards public certificate
+/*const char SECRET_CERTIFICATE[] = R"(
+-----BEGIN CERTIFICATE-----
+
+-----END CERTIFICATE-----
+)";*/
